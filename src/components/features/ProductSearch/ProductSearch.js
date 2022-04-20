b50a15a9bf1b9e6c7c175e6f87937fc12b353aaf
Serhii Sapiannyk <serhii09@icloud.com>
2022-04-20 12:43:53 +0200

    Fix code

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -6,34 +6,36 @@ import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-ico
 
 import styles from './ProductSearch.module.scss';
 
-const ProductSearch = () => (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <div className={styles.selectField}>
-        <ul>
-          <li>
-            Select a category
-            <ul>
-              <li>Bad</li>
-              <li>Sofa</li>
-              <li>Chair</li>
-              <li>Dining</li>
-              <li>Table</li>
-            </ul>
-          </li>
-        </ul>
+const ProductSearch = () => {
+  const selects = ['Bad', 'Sofa', 'Chair', 'Dining', 'Table'];
+
+  return (
+    <form action='' className={styles.root}>
+      <div className={styles.category}>
+        <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+        <div className={styles.selectField}>
+          <ul>
+            <li>
+              Select a category
+              <ul>
+                {selects.map(select => (
+                  <li key={selects.toString()}>{select}</li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
       </div>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-    </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-);
+      <div className={styles.searchField}>
+        <input placeholder='Search products...' type='text' />
+        <button>
+          <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        </button>
+      </div>
+    </form>
+  );
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
