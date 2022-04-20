2d8cc0eecfb39f66bc239b3afa90745c41769aea
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-04-20 11:14:15 +0200

    Repair utils function

--- src/utils/randomNumberForImg.js
+++ src/utils/randomNumberForImg.js
@@ -1,5 +1,1 @@
-export const randomNumberForImg = () => {
-  const random = parseInt(Math.floor(Math.random() * 5) + 1, 10);
-
-  return random;
-};
+export const randomNumberForImg = () => parseInt(Math.floor(Math.random() * 5) + 1, 10);
