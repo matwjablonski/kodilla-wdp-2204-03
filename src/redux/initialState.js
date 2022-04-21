b7492e92095467a903479d993b9f9d20e4116c07
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-04-21 13:28:53 +0200

    Add old price

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -12,6 +12,7 @@ const initialState = {
       name: 'Aenean Ru Bristique 1',
       category: 'bed',
       price: 30,
+      oldPrice: 40,
       stars: 2,
       promo: 'sale',
       newFurniture: true,
