2e1dd54524b5687e6e6f7fc04ec4b371783f21e9
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-04-27 16:23:45 +0200

    Repair

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -14,6 +14,7 @@ import { randomNumberForImg } from '../../../utils/randomNumberForImg';
 import FavoritesButton from '../../features/FavoriteButton/FavoriteButton';
 
 const ProductBox = ({
+  id,
   name,
   price,
   promo,
@@ -22,7 +23,6 @@ const ProductBox = ({
   addToCompare,
   oldPrice,
 }) => (
-
   <div className={styles.root}>
     <div className={styles.photo}>
       <img alt={name} src={'/images/furniture/' + randomNumberForImg() + '.jpg'} />
