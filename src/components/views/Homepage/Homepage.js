9d3cf22a8d9caf9039bfbc52703b57d5346ee4ae
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-04-30 15:28:41 +0200

    Add gallery view

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,13 +5,13 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
-import GaleryBox from '../../features/GaleryBox/GaleryBox';
+import GalleryBox from '../../features/GalleryBox/GalleryBox';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
     <NewFurniture />
-    <GaleryBox />
+    <GalleryBox />
   </div>
 );
 
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -12,11 +12,12 @@ const initialState = {
       name: 'Aenean Ru Bristique 1',
       category: 'bed',
       price: 30,
+      oldPrice: 40,
       stars: 2,
       promo: 'sale',
       newFurniture: true,
       isFavorite: true,
-      addToCompare: false,
+      addToCompare: true,
     },
     {
       id: 'aenean-ru-bristique-2',
